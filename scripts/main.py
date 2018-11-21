c2d45840876dffc06107fd1a63a11e035f23420e
Yu Xin Wu <yuxinwu@Yus-MacBook-Pro.local>
2018-11-21 10:33:39 -0500

    added unit test to check if pids of a trajectory are all the same

--- scripts/main.py
+++ scripts/main.py
@@ -39,12 +39,19 @@ if __name__=='__main__':
     t = test()
     for batch_size in range(1,4):
         traj_type = 1   
-        ret = datagen.get_Batch(list(data.values()),batch_size,traj_type,'image') #usage
+        ret = datagen.get_Batch(list(data.values()),batch_size,traj_type,'image')
         t.assertEqual(ret.shape,tuple((batch_size,traj_type+1))) #simple shape test
         
         traj_type = 2
         if(batch_size > 1): #Testing the case where batch size B > number of trajectories
             t.assertRaises(ValueError, datagen.get_Batch,list(data.values()),batch_size,traj_type,'image')
+        
+    ret = datagen.get_Batch(list(data.values()),3,1,'image')
     
+    for row in ret:
+        check = row[0].pid
+        for item in row:
+            t.assertEqual(check,item.pid) #make sure pid is the same throughout each timestep in a trajectory
+        
 
     
