498cacddd5a22f2f775f9e52ea466ece24069a03
surya1495 <9112765+suryatejadev@users.noreply.github.com>
2018-12-05 18:54:18 -0500

    Another clf experiment

--- disease_forecast/models/task.py
+++ disease_forecast/models/task.py
@@ -6,19 +6,23 @@ class ANN_DX(nn.Module):
     def __init__(self):
         super(ANN_DX, self).__init__()
         self.fc1 = nn.Linear(520, 400)
-        self.fc2 = nn.Linear(400, 200)
-        self.fc3 = nn.Linear(200, 100)
-        self.fc4 = nn.Linear(100, 3)
-        self.dp = nn.Dropout(p=0.0)
+        self.fc2 = nn.Linear(400, 400)
+        self.fc3 = nn.Linear(400, 400)
+        self.fc4 = nn.Linear(400, 100)
+        self.fc5 = nn.Linear(100, 3)
+        self.dp1 = nn.Dropout(p=0.2)
+        self.dp2 = nn.Dropout(p=0.5)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.dp(nn.BatchNorm1d(400)(x))
-        x = F.relu(self.fc2(x))
-        x = self.dp(nn.BatchNorm1d(200)(x))
-        x = F.relu(self.fc3(x))
-        x = self.dp(nn.BatchNorm1d(100)(x))
-        x = F.relu(self.fc4(x))
+        def layer(x, fc, dp, n):
+            x = F.relu(fc(x))
+            return dp(nn.BatchNorm1d(n)(x))
+        
+        x = layer(x, self.fc1, self.dp1, 400)
+        x = layer(x, self.fc2, self.dp2, 400)
+        x = layer(x, self.fc3, self.dp2, 400)
+        x = layer(x, self.fc4, self.dp1, 100)
+        x = F.relu(self.fc5(x))
         return x
 
     #  def __init__(self):
