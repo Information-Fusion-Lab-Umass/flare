c71cec677399f3a692b2527f0a9c6ce79a5890e0
surya1495 <9112765+suryatejadev@users.noreply.github.com>
2018-12-08 03:20:42 -0500

    T=2 1.5k epochs lstm

--- disease_forecast/models/temporal.py
+++ disease_forecast/models/temporal.py
@@ -20,19 +20,20 @@ class RNN(nn.Module):
         return x[:, -1, :]
 
 class LSTM(nn.Module):
-    def __init__(self):
+    def __init__(self, num_input, num_timesteps):
         super(LSTM, self).__init__()
-        
-    def forward(self, x): 
-        T = x.shape[1]
-        if T==1:
-            return x[:, 0, :] 
-        x = nn.LSTM(input_size = x.shape[2],
-                hidden_size = x.shape[2], 
+        self.T = num_timesteps
+        self.lstm = nn.LSTM(input_size = num_input,
+                hidden_size = num_input, 
                 num_layers = 1,
                 batch_first=True,
                 dropout=0.2, 
-                bidirectional=False)(x)[0]
+                bidirectional=False)
+        
+    def forward(self, x): 
+        if self.T==1:
+            return x[:, 0, :] 
+        x = self.lstm(x)[0]
         return x[:, -1, :]
 
 
